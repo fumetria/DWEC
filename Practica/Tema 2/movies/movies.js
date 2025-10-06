f1d4894a6fef074fbf66bba5d53f0599b5f6d316
Fu Jun <fulin.fujun@outlook.es>
2025-10-06 19:56:33 +0200

    Excercici Movies - Get, Create, Update i Delete funcionan amb mockapi. Refactoritzat IU.

--- Practica/Tema 2/movies/movies.js
+++ Practica/Tema 2/movies/movies.js
@@ -6,6 +6,8 @@ const moviesData = document.getElementById('movies-data');
 const refreshBtn = document.getElementById('refresh-btn');
 const mockBtn = document.getElementById('mock-btn');
 const formUpdateBtn = document.getElementById('form-update-btn');
+const loadingMockDataAnimation = document.getElementById('poblar-spin');
+const refreshDataAnimation = document.getElementById('update-spin');
 
 
 function buttonHidden(hidden, show) {
@@ -39,6 +41,7 @@ async function updateForm(movieId) {
 }
 
 async function insertMockData() {
+    loadingMockDataAnimation.classList.remove('hidden');
     const mockMovies = await fetch('./movies.json')
         .then(res => {
             if (res.ok) {
@@ -55,8 +58,8 @@ async function insertMockData() {
         movie.genre = movieGenre;
         const newMovie = createMovie(movie.title, movie.year, movie.director, movie.poster, movie.genre, movie.rate);
         await submitMovie(newMovie);
-
     }
+    loadingMockDataAnimation.classList.add('hidden');
 }
 
 async function updateMovie(movieTitle, movieYear, directorName, moviePoster, movieGenre, movieRate, movieId) {
@@ -90,13 +93,14 @@ async function deleteMovie(movieId) {
         method: 'DELETE',
     }).then(async res => {
         if (res.ok) {
-            moviesData.innerHTML = "";
-            await getMovies();
+            const movieRow = document.querySelector(`tr[data-movie-id="${movieId}"]`);
+            let removedRow = moviesData.removeChild(movieRow);
         }
     }).catch(err => {
         console.log('Algo salió mal al eliminar movie...')
     })
 }
+
 function getDate() {
     const date = Date.now();
     const today = new Date(date);
@@ -135,20 +139,20 @@ function createMovie(title, year, directorName, poster, genre, rate) {
     return newMovie;
 }
 
-function getStars(movieScore) {
+function getStars(movieRate) {
     const fillStar = '<i class="bi bi-star-fill"></i>';
     const halfStar = '<i class="bi bi-star-half"></i>';
     const emptyStar = '<i class="bi bi-star"></i>';
     let fillStars = 0;
     let halfStars = 0;
     let emptyStars = 0;
-    if (movieScore % 2 != 0) {
-        fillStars = (movieScore - 1) / 2;
+    if (movieRate % 2 != 0) {
+        fillStars = (movieRate - 1) / 2;
         halfStars = 1;
-        emptyStars = 5 - (halfStars + (movieScore / 2));
+        emptyStars = 5 - (halfStars + (movieRate / 2));
     } else {
-        fillStars = (movieScore) / 2;
-        emptyStars = 5 - (movieScore / 2);
+        fillStars = (movieRate) / 2;
+        emptyStars = 5 - (movieRate / 2);
     }
     let totalStars = "";
     for (let i = 0; i < fillStars; i++) {
@@ -187,14 +191,14 @@ async function getMovies() {
 
     movies.forEach((movie) => {
         moviesData.innerHTML += `
-        <tr class="even:bg-emerald-100 odd:bg-emerald-50">
+        <tr class="even:bg-emerald-100 odd:bg-emerald-50" data-movie-id="${movie.id}">
             <td class="px-2 py-1 text-center">${movie.id}</td>
             <td class="px-3 py-2 text-center"><img src="${movie.poster}" class="w-32 h-48 object-cover"></td>
-            <td class="px-2 py-1">${movie.title}</td>
+            <td class="px-2 py-1 text-wrap">${movie.title}</td>
             <td class="px-2 py-1 text-center">${movie.year}</td>
-            <td class="px-2 py-1 text-center">${movie.genre}</td>
+            <td class="px-2 py-1 text-center text-wrap">${movie.genre}</td>
             <td class="px-2 py-1 text-center">${movie.director}</td>
-            <td class="px-2 py-1text-center text-yellow-400">${movie.rate}</td>
+            <td class="px-2 py-1 text-center text-yellow-400">${movie.rate}</td>
             <td class="px-2 py-1 text-center">${movie.createdAt}</td>
             <td class="px-2 py-1 text-center">
             <div class="flex gap-3 justify-center text-lg px-2 py-1">
@@ -207,6 +211,7 @@ async function getMovies() {
         </tr>        
         `
     });
+
 }
 
 function fillTable(moviesArr) {
@@ -221,6 +226,13 @@ async function submitMovie(movie) {
         body: JSON.stringify(movie)
     }).then(res => {
         if (res.ok) {
+            title.value = "";
+            year.value = "";
+            director.value = "";
+            poster.value = "";
+            genre.value = "";
+            rate.value = "";
+            id.value = "";
             return;
         }
     }).catch(e => {
@@ -228,15 +240,18 @@ async function submitMovie(movie) {
     })
 }
 
-formBtn.addEventListener('click', (event) => {
+formBtn.addEventListener('click', async (event) => {
     event.preventDefault();
-    const newMovie = createMovie(title.value, genre.value, director.value, score.value);
-    submitMovie(newMovie);
+    const newMovie = createMovie(title.value, year.value, director.value, poster.value, genre.value, rate.value);
+    await submitMovie(newMovie);
+    await getMovies();
 });
 
 refreshBtn.addEventListener('click', async () => {
+    refreshDataAnimation.classList.remove('hidden');
     moviesData.innerHTML = "";
     await getMovies();
+    refreshDataAnimation.classList.add('hidden');
 })
 
 mockBtn.addEventListener('click', async () => {
