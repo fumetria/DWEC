c60608d00f0b2a3f2e659649d6de26aa23275ecc
Fu Jun <fulin.fujun@outlook.es>
2025-10-20 23:02:01 +0200

    Actividad Filmography - Mejora IU en GridFilms.jsx

--- Practica/Tema 3/filmography/src/components/GridFilms.jsx
+++ Practica/Tema 3/filmography/src/components/GridFilms.jsx
@@ -3,7 +3,10 @@ import Film from "./Film.jsx";
 export default function GridFilms({ moviesList }) {
   return (
     <>
-      <div id="movies-data" className="flex flex-wrap gap-4 my-5">
+      <div
+        id="movies-data"
+        className="flex flex-wrap justify-center gap-4 my-5"
+      >
         {moviesList.map((movie) => {
           return (
             <Film
