e09fa5d139e6c8b41c089079c15417befcd9c172
Fu Jun <fulin.fujun@outlook.es>
2025-10-27 21:18:07 +0100

    Filmography - Añadido URL de API y pasado por el componente FormNewFilm

--- Practica/Tema 3/filmography/src/App.jsx
+++ Practica/Tema 3/filmography/src/App.jsx
@@ -6,12 +6,13 @@ import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 
 function App() {
+  const URL = "https://68dc4aaa7cd1948060a9ef39.mockapi.io/api/v1/fuApi/films";
   return (
     <>
       <Header />
       <main>
         <section className="max-w-5xl mx-auto">
-          <FormNewFilm url={"#"} method={"GET"} />
+          <FormNewFilm url={URL} method={"POST"} />
           <GridFilms moviesList={peliculas} />
         </section>
       </main>
