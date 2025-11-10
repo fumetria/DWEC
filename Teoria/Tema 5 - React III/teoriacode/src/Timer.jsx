246bf98f679131ce0b334fe92815c228bc9bfe05
Fu Jun <fulin.fujun@outlook.es>
2025-11-10 18:25:02 +0100

    Tema 5 - React 3 codeExamples

---
+++ Teoria/Tema 5 - React III/teoriacode/src/Timer.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default function Timer() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <div>Time: {time}seconds</div>;
+}
